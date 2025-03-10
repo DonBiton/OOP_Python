420e822637defbc71db6d9dc76830dd086a3409c
Don Biton <miha1750@yandex.ru>
2025-03-10 09:20:17 +0300

    5_лень

--- 5_CL.py
+++ 5_CL.py
@@ -1,4 +1,6 @@
+import math
 class Fraction:
+    
     def __init__(self, numerator=1, denominator=1):
         self.numerator = numerator
         self.denominator = denominator
@@ -17,19 +19,25 @@ class Fraction:
         return f"{self.numerator}/{self.denominator}"
     
     def __add__(self, other):
-        print('Метод __add__ отрабатывает...')
-        # s1    = other
-        s1 = self.numerator
-        print(s1)   
+        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator
+        # if new_numerator % new_denominator == 0:
+
+        return Fraction(new_numerator, new_denominator)        
+        
+         
         
     # def exact_value(self):
 
-    def __mul__(self, other):
-        s1 = self.numerator
-        return Fraction(self.numerator *self.numerator, self.denominator)        
+    def __mul__(self, other): #Умножение
+        
+        return Fraction(self.numerator * other.numerator, self.denominator * other.denominator)  
+    def __truediv__(self, other): #Деление  
+          return Fraction(self.numerator * other.denominator, self.denominator * other.numuretor)  
 
 f1 = Fraction(2, 2)
 f2 = Fraction(3, 4)   
 print(f1)
 print(f2)
 print(f1 * f2) 
+
